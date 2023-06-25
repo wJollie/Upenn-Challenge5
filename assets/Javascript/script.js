d2b355a730b573c3bc1bfdfdfd471c3823db94b1
william jollie <william.jollie23@gmail.com>
2023-06-25 11:10:33 -0400

    -added 12 - 5 time boxes -added Js

--- assets/Javascript/script.js
+++ assets/Javascript/script.js
@@ -2,22 +2,40 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  // Display current date at the header of the page
+  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
+  $("#currentDay").text(currentDate);
+
+  // Click event listener for save button
+  $(".saveBtn").on("click", function () {
+    // id of parent block
+    var timeBlockId = $(this).parent().attr("id");
+    // get user input
+    var userInput = $(this).siblings(".description").val();
+    // save the user input to local storage
+    localStorage.setItem(timeBlockId, userInput);
+  });
+  // apply past,present,or future class
+  var currentHour = dayjs().hour();
+  $(".time-block").each(function () {
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+    // replace class when the hour passes
+    if (timeBlockHour < currentHour) {
+      $(this).removeClass("present future").addClass("past");
+    } else if (timeBlockHour === currentHour) {
+      $(this).removeClass("past future").addClass("present");
+    } else {
+      $(this).removeClass("past present").addClass("future");
+    }
+  });
+
+  // get the user input saved in local storage
+  $(".time-block").each(function () {
+    var timeBlockId = $(this).attr("id");
+    var userInput = localStorage.getItem(timeBlockId);
+
+    if (userInput) {
+      $(this).find(".description").val(userInput);
+    }
+  });
 });
